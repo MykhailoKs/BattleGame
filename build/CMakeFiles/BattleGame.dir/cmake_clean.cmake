346dbb1864675fa6d11906724976c6d2eea10e2f
MykhailoKs <ksyongik.mihaylo@gmail.com>
2025-01-02 21:02:11 +0100

    first commit

---
+++ build/CMakeFiles/BattleGame.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BattleGame"
+  "BattleGame.pdb"
+  "CMakeFiles/BattleGame.dir/src/main.cpp.o"
+  "CMakeFiles/BattleGame.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BattleGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
